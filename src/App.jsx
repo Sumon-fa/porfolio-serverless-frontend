5614d28869eceb3e986b6863c405ca41b81132ae
Sumon Fakir <fakirsumon78@gmail.com>
2022-04-24 04:46:03 +0300

    add ci pipeline

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import React, { Fragment } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import About from './components/layout/About';
-import Contact from './components/layout/Contact';
+
 import Home from './pages/resume/Home';
 function App() {
   return (
     <Fragment>
       <Routes>
         <Route path="/" element={<Home />} exact />
-      </Routes>
-      <Routes>
-        <Route path="/about" element={<About />} />
-      </Routes>
-      <Routes>
-        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Fragment>
   );
