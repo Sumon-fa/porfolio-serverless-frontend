6cca8cb61edc0c07bbf5081e0c9f0275539ef5a2
Sumon Fakir <fakirsumon78@gmail.com>
2022-04-27 18:19:41 +0300

    add version

--- src/components/style/contactStyle.js
+++ src/components/style/contactStyle.js
@@ -2,6 +2,6 @@ import { makeStyles } from '@material-ui/core';
 
 export const useStyles = makeStyles((theme) => ({
   section: {
-    margin: theme.spacing(10, 0, 8, 0),
+    margin: theme.spacing(10, 0, 0, 0),
   },
 }));
