480dfd04052f62bfdfcdc068ddbfca98d3d0a2b6
Sumon Fakir <fakirsumon78@gmail.com>
2022-04-28 11:38:48 +0300

    add footer

--- src/components/layout/Contact.jsx
+++ src/components/layout/Contact.jsx
@@ -1,90 +1,28 @@
-import {
-  Button,
-  Card,
-  CardContent,
-  Grid,
-  TextField,
-  Typography,
-} from '@mui/material';
+import * as React from 'react';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import resume from "../../assets/Sumon's Resume.pdf";
 import { useStyles } from '../style/contactStyle';
-import React from 'react';
-
-const Contact = () => {
+import { Link } from '@mui/material';
+export default function Contact() {
   const classes = useStyles();
-  return (
-    <Card
-      sx={{
-        maxWidth: '450px',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        padding: '20px 5px',
-      }}
-      className={classes.section}
-    >
-      <CardContent>
-        <Typography gutterBottom variant="h5">
-          Contact Me
-        </Typography>
-        <form>
-          <Grid container spacing={1}>
-            <Grid item xs="12" sm="6">
-              <TextField
-                label="First Name"
-                placeholder="Enter the first name"
-                variant="outlined"
-                fullWidth
-                required
-              ></TextField>
-            </Grid>
-            <Grid item xs="12" sm="6">
-              <TextField
-                label="Last Name"
-                placeholder="Enter the last name"
-                variant="outlined"
-                fullWidth
-                required
-              ></TextField>
-            </Grid>
-            <Grid item xs="12">
-              <TextField
-                type="email"
-                label="Email"
-                placeholder="Enter Email"
-                variant="outlined"
-                fullWidth
-                required
-              ></TextField>
-            </Grid>
-            <Grid item xs="12">
-              <TextField
-                type="number"
-                label="Phone"
-                placeholder="Enter Phone"
-                variant="outlined"
-                fullWidth
-              ></TextField>
-            </Grid>
-            <Grid item xs="12">
-              <TextField
-                label="Message"
-                placeholder="Type Your message here"
-                variant="outlined"
-                multiline
-                rows={4}
-                fullWidth
-                required
-              ></TextField>
-            </Grid>
-            <Grid item xs="12">
-              <Button type="submit" variant="outlined" fullWidth>
-                Submit{' '}
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </CardContent>
-    </Card>
-  );
-};
 
-export default Contact;
+  return (
+    <BottomNavigation
+      className={classes.photo}
+      sx={{ width: '100%', height: '200px', display: 'flex' }}
+    >
+      <Link
+        sx={{
+          margin: 'auto',
+          textDecoration: 'none',
+          color: '#fff',
+          fontSize: '2rem',
+        }}
+        href={resume}
+        download="Sumon's CV"
+      >
+        Download My CV
+      </Link>
+    </BottomNavigation>
+  );
+}
--- src/components/style/contactStyle.js
+++ src/components/style/contactStyle.js
@@ -1,7 +1,13 @@
-import { makeStyles } from '@material-ui/core';
+import { makeStyles } from '@mui/styles';
 
 export const useStyles = makeStyles((theme) => ({
   section: {
+    margin: theme.spacing(10, 0, 0, 0),
+  },
+  photo: {
+    backgroundImage: `linear-gradient(to bottom right,#04303140,#00606473),url("https://cdn-techno.konbini.com/fr/files/2021/04/fond-noir.jpg")`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
     margin: theme.spacing(10, 0, 0, 0),
   },
 }));
