cc72aa08daefb88d51b02fbcd580e4743f0c6c98
Sumon Fakir <fakirsumon78@gmail.com>
2022-04-30 19:16:00 +0300

    add responsive wide in header

--- src/components/style/headerStyle.js
+++ src/components/style/headerStyle.js
@@ -19,9 +19,16 @@ export const useStyles = makeStyles((theme) => ({
     fontWeight: '700 !important',
     fontFamily: 'Raleway !important',
     padding: theme.spacing(2, 0),
+    [theme.breakpoints.down('md')]: {
+      fontSize: '2.5rem !important',
+      padding: '10px !important',
+    },
   },
   headerContent: {
     fontSize: '2.5rem !important',
     fontWeight: '600 !important',
+    [theme.breakpoints.down('md')]: {
+      fontSize: '1.5rem !important',
+    },
   },
 }));
